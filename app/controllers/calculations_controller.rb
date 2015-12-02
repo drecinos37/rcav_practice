3bdbb847327a82f2c0a34801ff9c68230628290b
drecinos37 <drecinos@chicagobooth.edu>
2015-12-01 21:57:12 -0600

    Forgot to save

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,21 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+def sqrt
+
+  @the_number = params["zebra"].to_f
+ @the_sqrt = @the_number ** 0.5
+ render("sqrt.html.erb")
+end
+
+
+
+def random
+
+  @low_number = params["small"].to_f
+  @high_number = params["big"].to_f
+  @result_random = rand(@low_number..@high_number)
+ render("random.html.erb")
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:zebra", { :controller => "calculations", :action => "sqrt" })
+  get("/random/:small/:big", { :controller => "calculations", :action => "random" })
+
 end
